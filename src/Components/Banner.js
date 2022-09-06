f64a93dd7033dff1e11159c67bc1ca46cf46687c
pankajprajapati77 <pankajprajapati12527@gmail.com>
2022-09-07 00:31:50 +0530

    list and banner is complete

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -6,19 +6,21 @@ export default class Bannner extends Component{
         return (
             <>
             {movie == "" ? (
-                <div class="spinner-border text-danger" role="status">
-                <span class="visually-hidden">Loading...</span>
+                <div className="spinner-border text-danger" role="status">
+                <span className="visually-hidden">Loading...</span>
               </div>
             ) : (
-            <div className="card" >
-                <img src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`} className="card-img-top" alt="..."/>
-            <div className="card-body">
-                <h5 className="card-title">{movie.original_title}</h5>
-            <p className="card-text">
+            <div className="card banner-card" >
+                <img src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`} 
+                className="card-img-top banner-img" alt="..."/>
+            {/* <div className="card-body"> */}
+                <h5 className="card-title banner-title">
+                    {movie.original_title}</h5>
+            <p className="card-text banner-text">
                 {movie.overview}
                 </p>
             {/* <a href="#" className="btn btn-primary">Go somewhere</a> */}
-            </div>
+            {/* </div> */}
         </div>
         )}
         </>
